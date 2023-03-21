63d82d5be63e527ec9b04c7a15d0dcc3d14964f4
Retrobotics <perkiomenvalleyrobotics@gmail.com>
2023-03-21 16:00:24 -0400

    newcommit2023202000

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,7 +23,7 @@ public class Robot extends TimedRobot {
   //Objects
   public static DriveTrain drive = new DriveTrain();
   private OIHandler oi = new OIHandler();
-  private vision limelightboyo = new vision();
+  
   private balance ballet = new balance(); //see balance class
   private grabber grab = new grabber();
   public static arm armyBoy = new arm();
@@ -166,8 +166,6 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     grab.resetEncoders();
-    
-    limelightboyo.setPipeLine();
   }
 
   /** This function is called periodically during operator control. */
