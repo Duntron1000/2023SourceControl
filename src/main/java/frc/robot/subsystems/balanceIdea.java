21bc1504fc5d97b05b2202527b9619afa2760dd4
Liam Bruhin <lpbruhin@gmail.com>
2023-03-22 13:10:54 -0400

    wrote out a theory I have to balance

---
+++ src/main/java/frc/robot/subsystems/balanceIdea.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.controller.PIDController;
+
+import frc.robot.Robot.*;
+
+public class balanceIdea {
+    private AHRS navX;
+
+    private PIDController pid;
+
+    public balanceIdea(){
+        navX = new AHRS();
+        pid = new PIDController(0, 0, 0);
+    }
+
+    public void balance(DriveTrain d){ 
+        d.drive(0, pid.calculate(navX.getPitch(), 0));
+    }
+
+
+}
